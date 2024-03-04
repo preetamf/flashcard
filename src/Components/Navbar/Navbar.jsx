838ed4a3b7cfc45c2a8e7f53cead986b1f6cabdb
preetamf <preetamsinghe2202@gmail.com>
2024-03-04 22:58:46 +0530

    navbar created

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import DarkTheme from '../DarkTheme/DarkTheme'
+import Logo from './logo.png'
+
+const Navbar = () => {
+    return (
+        //issue fixed {remove css => fixed z-50 in line 8}
+        <div className='h-14 w-screen  bg-white dark:bg-[#1e2125] flex items-center space-between drop-shadow-md'>
+            <div className='flex items-center font-medium text-3xl'>
+                <img className='h-10 my-auto ml-4' src={Logo} alt="AlmaBetter" />
+                <span className='dark:text-white'>maBetter</span>
+            </div>
+            <DarkTheme />
+        </div>
+    )
+}
+
+export default Navbar
